76f9b213bf1230a7a9d1486a964562f235e01d3f
Randol Gonzalez <randol696@live.com>
2023-03-12 18:37:10 +0000

    switch case add

---
+++ javascript_class/switch.js
@@ -0,0 +1,23 @@
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+      console.log((answer = "alpha"));
+      break;
+    case 2:
+      console.log((answer = "beta"));
+      break;
+    case 3:
+      console.log((answer = "gamma"));
+      break;
+    case 4:
+      console.log((answer = "delta"));
+      break;
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
