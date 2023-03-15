ff5fadeb6ee9b014003074018254138485512142
Randol Gonzalez <randol696@live.com>
2023-03-15 14:49:53 +0000

    update

--- javascript_class/switch.js
+++ javascript_class/switch.js
@@ -46,3 +46,41 @@ function switchOfStuff(val) {
 }
 
 switchOfStuff(1);
+-----------
+
+
+function sequentialSizes(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val){
+    case 1:
+    case 2:
+    case 3:
+      answer ="Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer ="Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer ="High";
+      break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1);
+---
+function isLess(a, b) {
+  // Only change code below this line
+  return a <= b;
+  // Only change code above this line
+}
+
+isLess(10, 15);
