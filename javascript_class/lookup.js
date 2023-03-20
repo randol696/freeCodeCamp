9aea126ee186bfc74a75e2e9b542c121572f8ac0
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2023-03-20 09:30:33 -0500

    lookup added

---
+++ javascript_class/lookup.js
@@ -0,0 +1,14 @@
+const article = {
+  "title": "How to create objects in JavaScript",
+  "link": "https://www.freecodecamp.org/news/a-complete-guide-to-creating-objects-in-javascript-b0e2450655e8/",
+  "author": "Kaashan Hussain",
+  "language": "JavaScript",
+  "tags": "TECHNOLOGY",
+  "createdAt": "NOVEMBER 28, 2018"
+};
+
+const articleAuthor = article["author"];
+const articleLink = article["link"];
+
+const value = "title";
+const valueLookup = article[value];
