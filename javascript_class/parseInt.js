2b629410cc60952172af7c19a56a6c8e856b1365
Randol <randol@Randols-MBP-2.lan>
2023-03-22 11:38:45 -0500

    almost

---
+++ javascript_class/parseInt.js
@@ -0,0 +1,13 @@
+function convertToInteger(str) {
+    return parseInt(str);
+    
+}
+
+convertToInteger("56");
+//----------
+function convertToInteger(str) {
+    return parseInt(str, 2)
+  
+  }
+  
+  convertToInteger("10011");
---
+++ javascript_class/randomNumber.js
@@ -0,0 +1,26 @@
+function randomFraction() {
+
+    // Only change code below this line
+    
+  
+    return Math.random();
+  
+    // Only change code above this line
+  }
+  
+  randomFraction();
+  //random nomber multiply by 10 and rounding 
+
+  function randomWholeNum() {
+
+    // Only change code below this line
+  
+    return Math.floor(Math.random()*10);
+  }
+
+  //--------------------------------- givin values 
+  function randomRange(myMin, myMax) {
+    // Only change code below this line
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+    // Only change code above this line
+  }
