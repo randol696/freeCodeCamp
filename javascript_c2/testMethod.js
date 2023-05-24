3045d1cef352dfcf7f3d34dd84e4ccbc6fa4f118
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2023-05-24 17:18:42 +0000

    s

--- javascript_c2/testMethod.js
+++ javascript_c2/testMethod.js
@@ -1,3 +1,7 @@
 let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString);
+//
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // OR "posibles"
+let result = petRegex.test(petString);
