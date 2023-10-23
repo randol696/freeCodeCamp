fd4132718df7b12e5903c04335a46851df6017fd
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2023-10-23 20:55:37 +0000

    array object

---
+++ JavascriptOOP/add-the-own-properties-of-canary-to-the-array-own-props.js
@@ -0,0 +1,16 @@
+/* This Code has been create by: Randol */
+function Bird(name) {
+  this.name = name;
+  this.numLegs = 2;
+}
+
+let canary = new Bird("Tweety");
+let ownProps = [];
+// Only change code below this line
+for (let property in canary){
+  if(canary.hasOwnProperty(property)){
+    ownProps.push(property);
+  }
+}
+console.log(ownProps)
+
