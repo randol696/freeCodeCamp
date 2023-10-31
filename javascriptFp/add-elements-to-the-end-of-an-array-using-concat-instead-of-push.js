d6916b087078ac05f550bdcddfb4512ea52d65e1
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2023-10-31 13:24:26 +0000

    arrays

---
+++ javascriptFp/add-elements-to-the-end-of-an-array-using-concat-instead-of-push.js
@@ -0,0 +1,11 @@
+/* This Code has been create by: Randol */
+function nonMutatingPush(original, newItem) {
+  // Only change code below this line
+  return original.concat(newItem);
+
+  // Only change code above this line
+}
+
+const first = [1, 2, 3];
+const second = [4, 5];
+nonMutatingPush(first, second);
