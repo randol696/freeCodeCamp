646a8eb507fb3abe72261ab36a9104491597cacd
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2023-11-06 17:33:27 +0000

    putting into array

---
+++ JavascriptIntermediate/putting-into-array-each-letter-of-a-string.js
@@ -0,0 +1,11 @@
+/* This Code has been create by: Randol */
+function fearNotLetter(str) {
+  const alfabeth = [];
+  const letters ="abcdefghijklmnopqrstuvwxyz";
+  for (let i = 0; i < letters.length; i++){
+    alfabeth.push(letters[i]);
+  }
+  return alfabeth;
+}
+
+console.log(fearNotLetter("abce"));
