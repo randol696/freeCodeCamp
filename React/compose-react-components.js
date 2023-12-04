9575801a7dbcdc5c250886ac6431ac4bbf0055ad
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2023-12-04 18:33:47 +0000

    compose react components

---
+++ React/compose-react-components.js
@@ -0,0 +1,34 @@
+/* This Code has been create by: Randol */
+class Fruits extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h2>Fruits:</h2>
+        {/* Change code below this line */}
+        <NonCitrus />
+        <Citrus />
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+}
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        {/* Change code below this line */}
+        <Fruits />
+        {/* Change code above this line */}
+        <Vegetables />
+      </div>
+    );
+  }
+}
