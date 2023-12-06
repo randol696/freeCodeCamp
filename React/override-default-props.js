09e85775edbaf9651c458d1d912945a20a76fbe7
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2023-12-06 14:47:12 -0500

    default props use, and override default props

---
+++ React/override-default-props.js
@@ -0,0 +1,19 @@
+/* This Code has been create by: Randol */
+const Items = (props) => {
+    return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+  }
+  
+  Items.defaultProps = {
+    quantity: 0
+  }
+  
+  class ShoppingCart extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      { /* Change code below this line */ }
+      return <Items quantity={10} />
+      { /* Change code above this line */ }
+    }
+  };
---
+++ React/use-default-props.js
@@ -0,0 +1,10 @@
+/* This Code has been create by: Randol */
+const ShoppingCart = (props) => {
+    return (
+      <div>
+        <h1>Shopping Cart Component</h1>
+      </div>
+    )
+  };
+  // Change code below this line
+   ShoppingCart.defaultProps= {items: 0} 
