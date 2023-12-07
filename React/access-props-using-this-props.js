33dd3669febccc2dd4456df45b7d36acfadd0cbd
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2023-12-07 12:02:36 -0500

    access props using this.props / review props using stateless functional components

---
+++ React/access-props-using-this-props.js
@@ -0,0 +1,32 @@
+/* This Code has been create by: Randol */
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+  
+    }
+    render() {
+      return (
+          <div>
+              { /* Change code below this line */ }
+              <Welcome name="Jessica"/>
+              { /* Change code above this line */ }
+          </div>
+      );
+    }
+  };
+  
+  class Welcome extends React.Component {
+    constructor(props) {
+      super(props);
+  
+    }
+    render() {
+      return (
+          <div>
+            { /* Change code below this line */ }
+            <p>Hello, <strong>{this.props.name}</strong>!</p>
+            { /* Change code above this line */ }
+          </div>
+      );
+    }
+  };
---
+++ React/review-using-props-with-stateless-functional-components.js
@@ -0,0 +1,21 @@
+/* This Code has been create by: Randol */
+class CampSite extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <Camper/>
+        </div>
+      );
+    }
+  };
+  // Change code below this line
+  
+  const Camper = props => {
+  return <p>{props.name}</p>
+  };
+  
+  Camper.defaultProps={name: 'CamperBot'};
+  Camper.propTypes = {name: PropTypes.string.isRequired};
