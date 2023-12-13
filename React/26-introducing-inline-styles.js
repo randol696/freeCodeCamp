a5a1a46624489f69443935c57d373d8558c2ae7f
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2023-12-13 10:55:56 -0500

    26 introducing inline styles

---
+++ React/26-introducing-inline-styles.js
@@ -0,0 +1,8 @@
+/* This Code has been create by: Randol */
+class Colorful extends React.Component {
+    render() {
+      return (
+        <div style={{color: "red", fontSize: 72}}>Big Red</div>
+      );
+    }
+  };
