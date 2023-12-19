a7d59bb9c7c242b85c678f39befa93c206df467f
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2023-12-19 15:59:47 -0500

    35 give element unique key attribute

---
+++ React/35-give-sibling-elements-a-unique-key-attribute.js
@@ -0,0 +1,21 @@
+/* This Code has been create by: Randol */
+const frontEndFrameworks = [
+    'React',
+    'Angular',
+    'Ember',
+    'Knockout',
+    'Backbone',
+    'Vue'
+  ];
+  
+  function Frameworks() {
+    const renderFrameworks = frontEndFrameworks.map((item, index) => <li key={index}>{item}</li>); // Change this line
+    return (
+      <div>
+        <h1>Popular Front End JavaScript Frameworks</h1>
+        <ul>
+          {renderFrameworks}
+        </ul>
+      </div>
+    );
+  };
