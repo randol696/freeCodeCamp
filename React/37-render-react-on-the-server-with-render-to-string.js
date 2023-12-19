b2c297a20e8d4024cfbc00f064d352d49bf3da19
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2023-12-19 16:12:28 -0500

    37 react render on a server with a string

---
+++ React/37-render-react-on-the-server-with-render-to-string.js
@@ -0,0 +1,12 @@
+/* This Code has been create by: Randol */
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return <div/>
+    }
+  };
+  
+  // Change code below this line
+  ReactDOMServer.renderToString(<App />)
