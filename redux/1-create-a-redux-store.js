7936618ddf8c3c1435b1f3b5fc62e9e006d7886b
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2023-12-22 14:05:39 -0500

    new Folder Redux -> post create redux store, get state from the redux store'

---
+++ redux/1-create-a-redux-store.js
@@ -0,0 +1,9 @@
+/* This Code has been create by: Randol */
+const reducer = (state = 5) => {
+    return state;
+  }
+  
+  // Redux methods are available from a Redux object
+  // For example: Redux.createStore()
+  // Define the store here:
+  const store = Redux.createStore(reducer)
---
+++ redux/2-get-state-from-the-redux-store.js
@@ -0,0 +1,13 @@
+/* This Code has been create by: Randol */
+const store = Redux.createStore(
+    (state = 5) => state
+  );
+  
+  // Change code below this line
+  const currentState =store.getState()
+
+  /*
+
+The Redux store object provides several methods that allow you to interact with it. For example, you can retrieve the current state held in the Redux store object with the getState() method.
+
+The code from the previous challenge is re-written more concisely in the code editor. Use store.getState() to retrieve the state from the store, and assign this to a new variable currentState. */
