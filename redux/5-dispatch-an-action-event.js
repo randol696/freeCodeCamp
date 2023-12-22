41d7157505f2032992f6a28e5b84bf76f605a7b8
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2023-12-22 15:46:20 -0500

    5 dispatch and action event, 6 handle and action in the store

---
+++ redux/5-dispatch-an-action-event.js
@@ -0,0 +1,14 @@
+/* This Code has been create by: Randol */
+const store = Redux.createStore(
+    (state = {login: false}) => state
+  );
+  
+  const loginAction = () => {
+    return {
+      type: 'LOGIN'
+    }
+  };
+  
+  // Dispatch the action here:
+  store.dispatch(loginAction());
+  store.dispatch({type:'LOGIN'});
