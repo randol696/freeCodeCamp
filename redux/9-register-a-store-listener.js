8292fa116ff2b37966c7c606fd57ed3e51a9807f
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2023-12-26 11:10:43 -0500

    9 register a store listener

---
+++ redux/9-register-a-store-listener.js
@@ -0,0 +1,28 @@
+/* This Code has been create by: Randol */
+const ADD = 'ADD';
+
+const reducer = (state = 0, action) => {
+  switch(action.type) {
+    case ADD:
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(reducer);
+
+// Global count variable:
+let count = 0;
+
+// Change code below this line
+const add = () => count++
+store.subscribe(add)
+// Change code above this line
+
+store.dispatch({type: ADD});
+console.log(count);
+store.dispatch({type: ADD});
+console.log(count);
+store.dispatch({type: ADD});
+console.log(count);
