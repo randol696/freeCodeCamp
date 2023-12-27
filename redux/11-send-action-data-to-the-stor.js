05db964c4694cb1c42e7fe9216d0b69a0a62f83d
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2023-12-27 09:54:44 -0500

    11 send action data to the store

---
+++ redux/11-send-action-data-to-the-stor.js
@@ -0,0 +1,28 @@
+/* This Code has been create by: Randol */
+const ADD_NOTE = 'ADD_NOTE';
+
+const notesReducer = (state = 'Initial State', action) => {
+  switch(action.type) {
+    // Change code below this line
+    case ADD_NOTE:
+    return action.text;
+    // Change code above this line
+    default:
+      return state;
+  }
+};
+
+const addNoteText = (note) => {
+  // Change code below this line
+ return{
+   type:ADD_NOTE ,
+   text:note
+ }
+  // Change code above this line
+};
+
+const store = Redux.createStore(notesReducer);
+
+console.log(store.getState());
+store.dispatch(addNoteText('Hello!'));
+console.log(store.getState());
