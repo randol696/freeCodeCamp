47815a31802363cc04d7d740e3cb2125de42d96d
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2023-12-27 13:49:50 -0500

    1 getting started with react redux

---
+++ reactandredux/1-getting-started-with-react-redux.js
@@ -0,0 +1,15 @@
+/* This Code has been create by: Randol */
+class DisplayMessages extends React.Component {
+    // Change code below this line
+    constructor(props){
+      super(props);
+      this.state ={
+        input: '',
+        messages: []
+      }
+    }
+    // Change code above this line
+    render() {
+      return <div />
+    }
+  };
