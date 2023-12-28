d6f3792aa6a5acaa6d85bc80ff4e3a736ea1bc59
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2023-12-28 10:50:13 -0500

    6 map dispatch to props

---
+++ reactandredux/6-map-dispatch-to-props.js
@@ -0,0 +1,20 @@
+/* This Code has been create by: Randol */
+const addMessage = (message) => {
+    return {
+      type: 'ADD',
+      message: message
+    }
+  };
+  
+  // Change code below this line
+  
+    const mapDispatchToProps=(dispatch)=>{
+       return {
+         submitNewMessage:(message)=>{
+            dispatch(addMessage(message))
+         }
+       }
+     }  
+       
+     
+  
