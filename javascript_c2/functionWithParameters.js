2de126b61450ffcae4bd89ff4b725604cdbaf2a6
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2024-01-17 11:24:16 -0500

    funtionWidthParameters added

---
+++ javascript_c2/functionWithParameters.js
@@ -0,0 +1,9 @@
+function add(...arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum / arr.length;
+  }
+  
+  console.log(add(2.5, 4, 5));
